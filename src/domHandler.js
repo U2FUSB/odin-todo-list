526bd088353dd02eff962408a9af6e13339f6aac
LetsBuildAFlyingDonkey <LetsBuildAFlyingDonkey@github.com>
2025-11-28 19:29:48 +0100

    refactor: Remove some unused functions

--- src/domHandler.js
+++ src/domHandler.js
@@ -1,15 +1,5 @@
 import * as pubsub from "./pubsub.js";
-export {
-    createProject,
-    createTodo,
-    deleteProject,
-    deleteTodo,
-    getAllProjects,
-    getProject,
-    getTodo,
-    updateProject,
-    updateTodoProperty,
-};
+
 const domSections = (function () {
     const dynamicContentElement = document.querySelector(".dynamic-content");
     const projectsPageElement = document.createElement("section");
@@ -41,7 +31,6 @@ const domSections = (function () {
                     const projectName = document.querySelector([
                         "[data-todo-project]",
                     ]);
-                    console.log(projectName);
                     initialiseProjectsContentUi(projectName.value);
                     break;
             }
@@ -206,29 +195,6 @@ function displayTodoUi(todo) {
     buttonContainer.append(saveTodoSwitch, deleteTodoSwitch);
 }
 
-function displayTodoConsole(todo) {
-    if (todo === undefined) {
-        console.log("This todo does not exist");
-    } else {
-        console.log(`Title: ${todo.getTitle()}`);
-        console.log(`Description: ${todo.getDescription()}`);
-        console.log(`Assigned to: ${todo.getProject()}`);
-        console.log(`Is Done: ${todo.getIsDone()}`);
-    }
-    console.log("------");
-}
-
-function displayProjectConsole(project) {
-    console.log("||||||||||||||");
-    if (project === undefined) {
-        console.log("This project does not exist");
-    } else {
-        console.log(`Name: ${project.getName()}`);
-        console.log(`Contained Todos:`);
-        project.getTodos();
-    }
-    console.log("||||||||||||||");
-}
 function createTodo(title, project) {
     const todo = {
         title,
